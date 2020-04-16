77b2a84d94b4f12de73d74263a68098fef5361d3
nari.kim <nari.kim5252@gmail.com>
2020-04-16 22:27:03 +0900

    200416

--- JDBC/src/jdbc/day01/statement/JdbcTest04_Select_Where.java
+++ JDBC/src/jdbc/day01/statement/JdbcTest04_Select_Where.java
@@ -3,7 +3,7 @@ package jdbc.day01.statement;
 import java.sql.*;
 import java.util.*;
 
-public class JdbcTest03_select {
+public class JdbcTest04_Select_Where {
 
 	public static void main(String[] args) {
 		
@@ -46,48 +46,7 @@ public class JdbcTest03_select {
 			
 			// >>> 5. stmt 객체가 SQL문을 오라클서버에 보내서 실행되어지도록 한다.
 			rs = stmt.executeQuery(sql);
-			/*
-				stmt.executeQuery(sql);에서 파라미터인 sql은 오로지 select 문만 들어온다.
-				stmt.executeQuery(sql); 을 실행하면 select 되어진 결과물을 가져오는데
-				그 타입은 ResultSet 으로 가져온다.
-				
-				=== 인터페이스 ResultSet의 주요한 메소드 ===
-				1. next() 	: select 되어진 결과물에서 커서를 다음으로 올려주는 것 			리턴타입 boolean
-				2. first()	: select 되어진 결과물에서 커서를 가장 처음으로 옮겨주는 것		리턴타입 boolean
-				3. last() 	: select 되어진 결과물에서 커서를 가장 마지막으로 옮겨주는 것 		리턴타입 boolean
-				
-				== 커서가 위치한 행에서 컬럼의 값을 읽어들이는 메소드
-				getInt(숫자) : 컬럼타입이 숫자이면서 정수로 읽어들일때
-								파라미터 숫자는 컬럼의 위치 값
-				
-				getInt(문자) : 컬럼의 타입이 숫자이면서 정수로 읽어들이때
-			                  	파라미터 문자는 컬럼명 또는 alias명 
-			                  
-			   getLong(숫자) : 컬럼의 타입이 숫자이면서 정수로 읽어들이때
-			                    	파라미터 숫자는 컬럼의 위치값 
-			                 
-			   getLong(문자) : 컬럼의 타입이 숫자이면서 정수로 읽어들이때
-			                     	파라미터 문자는 컬럼명 또는 alias명                
-			   
-			   getFloat(숫자) : 컬럼의 타입이 숫자이면서 실수로 읽어들이때
-			                      	파라미터 숫자는 컬럼의 위치값 
-			                 
-			   getFloat(문자) : 컬럼의 타입이 숫자이면서 실수로 읽어들이때
-			                     	파라미터 문자는 컬럼명 또는 alias명 
-			                      
-			   getDouble(숫자) : 컬럼의 타입이 숫자이면서 실수로 읽어들이때
-			                       	파라미터 숫자는 컬럼의 위치값 
-			                 
-			   getDouble(문자) : 컬럼의 타입이 숫자이면서 실수로 읽어들이때
-			                     	파라미터 문자는 컬럼명 또는 alias명    
-			                        
-			   getString(숫자) : 컬럼의 타입이 문자열로 읽어들이때
-			                     	파라미터 숫자는 컬럼의 위치값 
-			                 
-			   getString(문자) : 컬럼의 타입이 문자열로 읽어들이때
-			                     	파라미터 문자는 컬럼명 또는 alias명     
-				
-			 */
+			
 			
 			System.out.println("------------------------------------------------------");
 			System.out.println(" 글번호\t글쓴이\t글내용\t\t작성일자");
@@ -105,7 +64,12 @@ public class JdbcTest03_select {
 				
 				System.out.println(no + "\t" +name+ "\t" +msg+ "\t"+writeday);
 
-			}		
+			} // end of while (rs.next())--------------------
+			
+			//////////////////////////////////////////////////////////////////////////////
+			// === where 절에 해당하는 메뉴 보여주기
+			
+			
 
 			
 		} catch (ClassNotFoundException e) {
@@ -126,7 +90,7 @@ public class JdbcTest03_select {
 		}
 		
 		sc.close();
-		System.out.println(" ~~~~~~ 종료 ~~~~~~");
+		System.out.println(" ~~~~~~ 프로그램 종료 ~~~~~~");
 
 	} // end of void main()-------------------------------
 
